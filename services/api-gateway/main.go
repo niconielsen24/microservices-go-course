151f4d1431a1c4410bef890bfd386666b4365c75
Nicolas Nielsen <niconielsen.pc@gmail.com>
2026-05-03 18:21:40 +0000

    Lesson 32 - Section 6

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /trip/preview", handleTripPreview)
+	mux.HandleFunc("POST /trip/preview", enableCORS(handleTripPreview))
 	mux.HandleFunc("/ws/drivers", handleDriverWebsocket)
 	mux.HandleFunc("/ws/riders", handleRiderWebsocket)
 
